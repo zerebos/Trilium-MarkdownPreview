d2feb202b45da1a4850f3e19cb79f17689edabca
Zack Rauen <rauenzi@outlook.com>
2023-08-15 23:41:57 -0400

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # Trilium-MarkdownPreview
 A widget for trilium notes for live previewing markdown files with support for anchors, images, and sync scroll.
+
+## Preview
+<!-- https://raw.githubusercontent.com/rauenzi/Trilium-MarkdownPreview/blob/main/LICENSE -->
+![Banner](https://i.imgur.com/9shsoST.png)
+
+
+## Features
+
+- Preview markdown notes in real time
+- Global and per-note styles
+- Embed local images
+- Clickable in-page anchor links
+- Syncable scrollbars
+- GFM-compliant with tables and more
+
+
+## Installation
+
+1. Create a new code note with `JS Frontened` type. Copy and paste [widget.js](https://github.com/rauenzi/Trilium-SyntaxHighlight/blob/main/src/widget.js) into the note.
+    - Be sure to add the `#widget` attribute to the note
+1. Attach the [marked.min.js](https://github.com/rauenzi/Trilium-SyntaxHighlight/blob/main/lib/marked.min.js) file to the note either via drag-n-drop or import.
+1. (optional) Create one (or more) child code notes of `CSS` type and fill it with any global markdown styles.
+
+### Syntax Highlighting (optional)
+If you want your codeblocks to have syntax highlighting, follow these steps:
+
+1. Attach the [highlight.min.js](https://github.com/rauenzi/Trilium-SyntaxHighlight/blob/main/lib/highlight.min.js) file to the note either via drag-n-drop or import.
+1. (optional) Attach the pre-made syntax styles from [styles.css](https://github.com/rauenzi/Trilium-SyntaxHighlight/blob/main/src/styles.css) as a child note of the [main widget](#installation).
+
+
+## Usage
+
+To make a note previewable, simply add the `#markdownPreview` attribute to it. (You may have to switch notes after). After that, just start writing in markdown on the code editor side. You'll see the preview update with you as you go.
+
+If you're going to add any images or styles to the note, it can be annoying to see the preview cards at the bottom. You can hide those by adding the `#hideChildrenOverview` native to Trilium.
+
+### Images
+#### Local
+
+To use a local image, attach the image as a child of the markdown note. You can then simply refer to it by filename.
+
+#### Remote
+
+Remote images can be attached just like any other image using the full url.
+
+### Anchor Links
+
+The actual heading anchors are created automatically and match GFM style. That means for complex headings you can have multiple `-` in a row. e.g. `Heading (subtext)` becomes `#heading--subtext-`.
+
+From there you can create anchor links by linking them as `[link text](#heading-name)` replacing any special characters (and spaces) with `-`.
+
+### Scroll Sync
+
+By default, MarkdownPreview will try to keep the scroll bars in sync no matter which side you are scrolling. The widget is not perfect at keeping them exactly at the same place contextually because the rendering can be so different.
+
+#### Disabling
+
+You can disable the sync entirely by adding the `#markdownScrollSync` attribute with `none` as the value. You can also disable each side separately with `left` or `right`. For instance, if you want the preview to automatically scroll as you scroll the source, but not the other way around, then use `#markdownScrollSync=left`.
+
+### Styling
+
+Both the global and local styles are picked up and reapplied on note switch, so no need to keep reloading if you are tinkering with your styles!
+#### Global
+As mentioned in the [installation](#installation) section, any `CSS` note that is a child to the widget code will automatically be picked up and applied.
+
+#### Local
+Local styles are specific to the markdown note. Just make a `CSS` note as a child of the markdown note and it'll be picked up!
+
+## Showcase
+
+### Anchors
+
+
+### Local Styles
+
+
+### ScrollSync
+
+
+### ScrollSync=left
+
---
+++ src/widget.js
@@ -0,0 +1,164 @@
+/**
+ * This widget allows you to preview any markdown code note in real time.
+ */
+const TPL = `<div style="padding: 10px; border-left: 0px solid var(--main-border-color); height: 100%;">
+<style></style>
+<div id="markdown-preview"></div>
+</div>`;
+
+/* global highlightminjs:false markedminjs:false */
+
+
+// A couple of temporary hoists
+let noteId = "";
+const imageCache = {};
+
+
+/** @type {import("highlight.js").default} */
+let hljs;
+try {
+    hljs = highlightminjs;
+    hljs.configure({cssSelector: "#markdown-preview pre code"});
+}
+catch {
+    // eslint-disable-next-line no-console
+    console.info("highlight.min.js not found, no syntax highlighting in markdown preview.");
+}
+
+/** @type {import("marked").Marked} */
+const markedjs = markedminjs;
+
+const slugify = text => text.toLowerCase().replace(/[^\w]/g, "-");
+const renderer = {
+  heading(text, level) {
+    const escapedText = slugify(text);
+    return `<h${level}>
+              <a href="${escapedText}" id="${escapedText}" class="anchor">
+                <span class="header-link"></span>
+              </a>
+              ${text}
+            </h${level}>`;
+  },
+    
+    image(href, title, text) {
+        const found = imageCache[noteId] && imageCache[noteId][href];
+        if (found) href = `api/images/${found}/${href}`;
+        return `<img src="${href}" title="${title}" alt="${text}">`;
+    },
+};
+
+markedjs.use({renderer});
+
+
+class MarkdownPreviewWidget extends api.RightPanelWidget {
+    get widgetTitle() {return "Markdown Preview";}
+    get parentWidget() {return "right-pane";}
+
+    isEnabled() {
+        return super.isEnabled()
+            && this.note.type === "code"
+            && this.note.mime
+            && this.note.mime === "text/x-markdown"
+            && this.note.hasLabel("markdownPreview");
+    }
+
+    async doRenderBody() {
+        this.$body.html(TPL);
+        this.$preview = this.$body.find("#markdown-preview");
+        this.$body.on("click", "a", this.jumpToLink.bind(this));
+        await this.updateCss();
+        return this.$body;
+    }
+
+    async refreshWithNote(note) {
+        const {content} = await note.getNoteComplement();
+        this.$preview.html(markedjs.parse(content));
+        hljs?.highlightAll?.();
+    }
+
+    async entitiesReloadedEvent({loadResults}) {
+        if (loadResults.isNoteContentReloaded(this.noteId)) {
+            this.refresh();
+        }
+    }
+    
+    async noteSwitched() {
+        noteId = this.note.noteId;
+        await this.getImages();
+        await this.updateCss();
+        await this.refresh();
+
+        if (!this.isEnabled()) return;
+        const scrollSyncStatus = this.note.getLabelValue("markdownScrollSync");
+        const disableBoth = scrollSyncStatus === "none";
+        const onlyLeft = scrollSyncStatus === "left";
+        const onlyRight = scrollSyncStatus === "right";
+        
+        $("#center-pane .CodeMirror-scroll, #center-pane .component.scrolling-container").off(".mdpreview");
+        $("#right-pane").off(".mdpreview");
+        if (disableBoth) return;
+        
+        const isFullHeight = document.querySelector(".note-detail.full-height");
+        const centerSelector = isFullHeight ? "#center-pane .CodeMirror-scroll" : "#center-pane .component.scrolling-container";
+        const center = document.querySelector(centerSelector);
+        const right = document.querySelector("#right-pane");
+        if (!onlyRight) this.addSyncListener(center, right, !onlyLeft);
+        if (!onlyLeft) this.addSyncListener(right, center, !onlyRight);
+    }
+    
+    async getImages() {
+        const children = await this.note.getChildNotes();
+        const images = children.filter(n => n.type === "image");
+        imageCache[this.note.noteId] = {};
+        for (const image of images) imageCache[this.note.noteId][image.title] = image.noteId;
+    }
+    
+    jumpToLink(ev) {
+        const link = ev.target;
+        const href = link.getAttribute("href");
+        const isAnchorLink = href.startsWith("#");
+        if (!isAnchorLink) return;
+        const heading = this.$body.find(href);
+        if (!heading.length) return;
+        heading[0].scrollIntoView({behavior: "instant", block: "start"});
+    }
+    
+    addSyncListener(target, other, shouldRestart = true) {
+        const jTarget = $(target);
+        jTarget.on("scroll.mdpreview", (ev) => {
+            $(other).off("scroll.mdpreview");
+            this.syncedScroll(ev.target, other);
+            clearTimeout($.data(jTarget, "scrollTimer"));
+            $.data(jTarget, "scrollTimer", setTimeout(() => {
+                if (shouldRestart) this.addSyncListener(other, target, shouldRestart);
+            }, 250));
+        });
+    }
+    
+    syncedScroll(target, other) {
+        const max = target.scrollHeight - target.clientHeight;
+        const current = target.scrollTop;
+        const percent = current / max;
+        const maxPane = other.scrollHeight - other.clientHeight;
+        const newScroll = (percent * maxPane);
+        other.scrollTop = newScroll;
+    }
+    
+    async updateCss() {
+        let accumulator = "";
+
+        const globalChildren = await api.startNote.getChildNotes();
+        const globalStyles = globalChildren.filter(n => n.mime == "text/css");
+        for (const style of globalStyles) accumulator += await style.getContent();
+
+        if (this.note) {
+            const localChildren = await this.note.getChildNotes();
+            const localStyles = localChildren.filter(n => n.mime == "text/css");
+            for (const style of localStyles) accumulator += await style.getContent();
+        }
+
+        this.$body.find("style").html(accumulator);
+    }
+}
+
+module.exports = new MarkdownPreviewWidget();
